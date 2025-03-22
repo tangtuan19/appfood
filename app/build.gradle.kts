123fea2e802b530b6b748c5ee054c1d56ea67870
tangtuan19 <tangtuan1901@gmail.com>
2025-03-22 10:21:56 +0700

    add homefragment

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,4 +51,20 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    //new
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.cardview:cardview:1.0.0")
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
+    implementation("pl.droidsonroids.gif:android-gif-drawable:1.2.27")
+
+
+    //intuit
+    implementation("com.intuit.sdp:sdp-android:1.0.6")
+    implementation("com.intuit.ssp:ssp-android:1.0.6")
+
+    //animation
+    implementation("pl.droidsonroids.gif:android-gif-drawable:1.2.17")
+
+
 }
